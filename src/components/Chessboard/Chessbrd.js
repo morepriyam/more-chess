e943c3fa6b9fe5d6d91a70243d83f5d6e1bdaf2b
Priyam More <morepriyam99@gmail.com>
2023-08-02 23:15:37 +0530

    Update Chessbrd.js

--- src/components/Chessboard/Chessbrd.js
+++ src/components/Chessboard/Chessbrd.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import PropTypes from "prop-types";
 import { Chess } from "chess.js"; // import Chess from  "chess.js"(default) if recieving an error about new Chess() not being a constructor
 import Chessboard from "chessboardjsx";
